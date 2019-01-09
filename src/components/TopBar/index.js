b1d7e8aa2ad9505d8d8bebbf58ed457f5c9fbc5c
judy <jss0125@163.com>
2019-01-09 11:24:19 +0800

    app.css 公共样式

--- src/components/TopBar/index.js
+++ src/components/TopBar/index.js
@@ -1,15 +1,15 @@
 import React,{Component} from 'react'
+import './index.scss'
 
 class TopBar extends Component{
 	render(){
-		return <div>
-			
+		return (
+			<header className='header'>
+				<p>分类</p>
 
-
-			TopBar
-
-			
-		</div>
+						
+			</header>
+		)
 	}
 }
 
