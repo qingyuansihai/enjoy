e966fe159d75c5840d0961e359edc20a7a453179
Seastyle <1024644766@qq.com>
2019-01-09 17:37:09 +0800

    newcata

--- src/view/Category/Categorylistitem/index.js
+++ src/view/Category/Categorylistitem/index.js
@@ -1,30 +1,50 @@
 import React,{Component} from 'react';
 import axios from 'axios';
+import './index.scss';
 
 class Categorylistitem extends Component{
 	constructor(props) {
 	  super(props);
 	
 	  this.state = {
-	  	datalist: null
+	  	datalist: []
 	  };
 	}
 
 	componentDidMount() {
-		// axios ({
-		// 	url:`https://api.ricebook.com/4/tab/sub_category.json?category_id=${this.props.params.cateid}&city_id=104&from_id=0`
-		// }).then(res=>{
-		// 	console.log(res)
-		// 	this.setState({
-		// 		datalist: res
-		// 	})
-		// })
+		axios ({
+			url:`https://api.ricebook.com/4/tab/category_product_list.json?category_id=${this.props.match.params.cateid}&sort=1&from_id=0&city_id=104&page=0`
+		}).then(res=>{
+			console.log(res.data)
+			this.setState({
+				datalist: res.data
+			})
+		})
 	}
+
 
 	render(){
 		return <div>
-		!!!!!!!!!!!!!!!!!!!
-			{this.state.datalist}
+			<ul className="navlist">
+				<li className="navleft">全部</li>
+				<li className="navright">智能排序<span className="tridown"></span></li>
+			</ul>
+			<ul className="downlist">
+				{
+					this.state.datalist.map(item => 
+						<li className="downdata">
+							<img src={item.product_image}/>
+							<div className="itemdown">
+								<p className="listup">{ item.name }</p>
+								<p className="listdown">
+									<span className="priceleft">{ item.price/100+"/2位" }</span>
+									<span className="priceright">{ "￥"+item.original_price/100 }</span>
+								</p>
+							</div>
+						</li>
+					)
+				}
+			</ul>
 		</div>
 	}
 }
