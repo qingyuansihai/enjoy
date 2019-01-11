ed1e93b231b61ace3642efc1c3e0515cde9ad3aa
liyj <1983918044@qq.com>
2019-01-11 19:36:36 +0800

    1.0.1--修改bug

--- src/view/Product/model.js
+++ src/view/Product/model.js
@@ -13,10 +13,20 @@ function getProductDetailInfo(product_id) {
 	return axios({
 		url: `https://api.ricebook.com/product/info/product_detail.json?product_id=${product_id}`,
 	}).then(res => {
+		switch (res.data.basic.product_id) {
+			case 1042633:
+				return getProductDetailInfo(1024078);
+			case 1043819:
+				return getProductDetailInfo(1024078);
+			case 1035976:
+				return getProductDetailInfo(1035777);
+			default:
+		}
+
 		if (res.data.basic.product_id === 1024077) {
-			console.log(11111)
 			res.data.modules.shift()
 		}
+
 		return res.data.modules
 	})
 }
