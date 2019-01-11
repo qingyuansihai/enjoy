0424f5d1286732f5b43b238c9eff5819ed0426fb
liyj <1983918044@qq.com>
2019-01-11 12:44:02 +0800

    评价

--- src/view/Feedback/index.js
+++ src/view/Feedback/index.js
@@ -1,13 +1,110 @@
 import React,{Component} from 'react'
+import './index.scss'
+import getFeedback from './model'
+import {connect} from 'react-redux'
 
 class Feedback extends Component{
+	constructor(props) {
+		super(props);
+		this.state = {
+			isChecked: 0,
+			new: [],
+		};
+		this.page = 0
+	}
+
 	render() {
 		return (
-			<div>
+			<div id="all-feedback">
+				<div className="feed-tabs">
+					<span className={this.state.isChecked===0 ? 'checked' : ''} onClick={this.handleNew.bind(this)}>最新</span>
+					<span className={this.state.isChecked===1 ? 'checked' : ''} onClick={this.handleImage.bind(this)}>有图</span>
+				</div>
 
+				<ul className="feed-list">
+					{
+						this.state.new.map(item =>
+							<li key={item.id}>
+								<div className="user">
+									<img src={item.avatar} alt=""/>
+									<span>{item.nick_name}</span>
+								</div>
+								<div className="score">
+									{
+
+										item.scores.map(item =>
+											<span key={item.id}>{item.name} {item.score.toFixed(1)}</span>
+										)
+									}
+								</div>
+								<p className="user-text">{item.text}</p>
+								<div className="image">
+									{
+										item.images.map((item, index) =>
+											<img src={item} alt="" key={index}/>
+										)
+									}
+								</div>
+							</li>
+						)
+					}
+
+				</ul>
 			</div>
 		);
 	}
+
+	handleNew() {
+		getFeedback(this.props.match.params.id, false, this.page).then(res => {
+			console.log(res);
+			this.setState({
+				isChecked: 0,
+				new: res
+			})
+		});
+	}
+	handleImage() {
+		getFeedback(this.props.match.params.id, true, this.page).then(res => {
+			console.log(res);
+			this.setState({
+				isChecked: 1,
+				new: res
+			})
+		});
+	}
+
+
+	componentDidMount() {
+		getFeedback(this.props.match.params.id, false, this.page).then(res => {
+			console.log(res);
+			this.setState({
+				new: res
+			})
+		});
+
+		this.props.hideNavBar()
+	}
+
+	componentWillUnmount() {
+		this.props.showNavBar()
+	}
 }
 
-export default Feedback
+export default connect(
+	null,
+	{
+		showNavBar(){
+			return {
+				type:"ShowNavBar",
+				payload:true  // payload 可以随便起名字
+			}
+		},
+
+		hideNavBar(){
+			return {
+				type:"HideNavBar",
+				payload:false  // payload 可以随便起名字
+			}
+		}
+	}
+)(Feedback)
